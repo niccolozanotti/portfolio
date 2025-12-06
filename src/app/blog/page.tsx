2e0e321d7e37f688b93be6030eee189d3290b125
francesco ercolani <francesco.ercolani3@studio.unibo.it>
2025-12-06 20:57:11 +0100

    Add empty blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,10 @@
+export default function BlogPage() {
+  return (
+    <div className="p-8 space-y-4">
+      {/* Mostra la barra degli indirizzi */}
+
+      <h1 className="text-3xl font-bold">Blog</h1>
+      <p>Benvenuto nella sezione Blog del portfolio!</p>
+    </div>
+  );
+}
