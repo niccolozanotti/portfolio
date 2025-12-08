10b4fe8dfc2c82980f0f1285a377623e9aa92fca
francesco ercolani <francesco.ercolani3@studio.unibo.it>
2025-12-08 14:54:58 +0100

    Add projects page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function ProjectsPage() {
+  return (
+    <div className="p-8 space-y-4">
+      <h1 className="text-3xl font-bold">Projects</h1>
+      <p>Welcome to projects page</p>
+    </div>
+  );
+}
