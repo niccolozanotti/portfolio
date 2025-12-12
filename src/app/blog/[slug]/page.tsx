fd08b7f0f83105e33b57ee54b2dbb8480515a623
francesco ercolani <francesco.ercolani3@studio.unibo.it>
2025-12-12 09:41:03 +0100

    Add directory and page for single post content view

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,21 @@
+import { getPostBySlug } from "@/lib/server/utils";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import { useMDXComponents } from "mdx-components";
+
+const components = useMDXComponents({});
+type Props = {
+    params: { slug: string };
+};
+
+export default async function BlogPostPage({ params }: Props) {
+    const resolvedParams = await params;
+    const { slug } = resolvedParams;
+
+    const { frontmatter, content } = getPostBySlug(slug);
+
+    return (
+        <article className="prose py-8">
+            <MDXRemote source={content} components={components} />
+        </article>
+    );
+}
